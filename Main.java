2e997a7765c3e47cab111454e439b0718d656ca0
ICOM STUDENTS <yetsiel.aviles@upr.edu>
2018-02-19 13:21:25 -0400

    Area Calculation.

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
-
-
-public static void main(String args[]) {
-
-
-
-System.out.println("Hello World");
+	public static void main() {
+		double radius;
+		double area;
+		double perimeter;
+		
+		Scanner x = new Scanner(System.in);
+		radius = x.nextDouble();
+		area = Math.PI*Math.pow(radius, 2);
+		perimeter = 2*Math.PI*radius;
+		System.out.println("Half Area:"+ area/2);
+		
+	}
 }
-}
+
+
 
